058dffe3efc73d884acf2955044bc7134fbf4396
Roy Rinberg <royrinberg@gmail.com>
2020-08-08 20:12:55 -0700

    create graph of authors, looking at shared papers as edge weights. Add graphing capabilities

--- arxiv_graphs/investigations.py
+++ arxiv_graphs/investigations.py
@@ -2,8 +2,9 @@
 import arxiv
 import pandas as pd
 import queue
-
-query = arxiv.query("Alexandra Chouldechova", max_results = 10)
+from collections import defaultdict
+import networkx as nx
+import matplotlib.pyplot as plt
 
 # TODO : test discovery BFS Traversal
 
@@ -81,11 +82,60 @@ def BFS_author_query(original_author, max_search_results = 10, max_depth = 5):
     discovery_BFS_traversal(original_author, max_depth= max_depth, next_traversal_f = next_traversal_vertices_partially_applied)
     return all_articles[0]
 
+def get_authors_to_articles(all_articles):
+    """ 
+    input : dataframe of all articles
+    returns : dict of author_name to article ids
+    """
+    author_to_articles = defaultdict(lambda:list)
+    for i, row in all_articles.iterrows():
+        for author in row.authors:
+            author_to_articles[author].append(row.article)
+    return author_to_articles
+
+def create_edge(author1, author2, G):
+    if G.has_edge(author1, author2):
+        G[author1][author2]['weight']+=1
+    else:
+        G.add_edge(author1, author2, weight= 1)
+
+def generate_author_graph(all_articles):
+    """ return networkx graph of all the authors, with weights between them based on their shared papers"""
+    G = nx.Graph()
+    for i, row in all_articles.iterrows():
+        for i in range(len(row.authors)):
+            for j in range(i+1, len(row.authors)):
+                create_edge(row.authors[i], row.authors[j], G)
+    return G
+
+def plot_weighted_graph(G):
+    """Plot a weighted graph"""
+
+    pos=nx.spring_layout(G) 
+    nx.draw_networkx_nodes(G,pos,node_color='green')#,node_size=700)
+    
+    all_weights_sum = sum([G[e[0]][e[1]]['weight'] for e in G.edges])
+    nx.draw_networkx_labels(G, pos=pos)
+    # this seems wrong, but for now - individually plot each edge 
+    for edge in G.edges:
+        weight = G[edge[0]][edge[1]]['weight']
+        width = weight*len(G.nodes)*10.0/all_weights_sum
+        nx.draw_networkx_edges(G,pos,edgelist=[edge],width=width)
+ 
+    plt.show() 
+
+
+# example for weighted graph drawing: https://networkx.github.io/documentation/stable/auto_examples/drawing/plot_weighted_graph.html
 if __name__ == "__main__":
-    articles = BFS_author_query(original_author = "Alexandra Chouldechova", max_search_results= 3, max_depth= 2)
+    articles = BFS_author_query(original_author = "Alexandra Chouldechova", max_search_results= 5, max_depth= 2)
 
     # generate a graph of authors, where the weight is the number of papers shared
-    
+    G = generate_author_graph(articles)
+    #plot_weighted_graph(G)
+    if True:
+        pos = nx.spring_layout(G)  # positions for all nodes
+        nx.draw(G, with_labels=True, font_weight='bold')
+        plt.show()
     print(len(articles))
     
     
