66e2b07eaf5391e16844cf1440ecfca59c942d5f
Roy Rinberg <royrinberg@gmail.com>
2020-08-09 12:00:59 -0700

    documenting pre-commit logic a bit more

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 GIT_HOME=$(git rev-parse --show-toplevel)
+# clean the jupyter notebooks, to make sure that they don't bloat
 nbstripout $GIT_HOME/arxiv_traverser/*ipynb
+# apply yapf on python files
 python -m yapf -i $GIT_HOME/arxiv_traverser/*py
+# add all the changes to the commit
 git add $GIT_HOME/arxiv_traverser/*py
 git add $GIT_HOME/arxiv_traverser/*ipynb
